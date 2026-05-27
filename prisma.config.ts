bf568e94bec70eeddf0ef615fa699174eeed8edb
BlueStarVN101 <tinleducdev@gmail.com>
2026-05-27 15:28:11 +0700

    update prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    // url: env("DATABASE_URL"),
-    url: process.env.DATABASE_URL,
+    url: env("DATABASE_URL"),
   },
 });
